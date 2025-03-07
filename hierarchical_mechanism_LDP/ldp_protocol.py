59a127f1210521520adc636c8c9dd1616283c6df
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-03-07 22:00:49 +0100

    mod: add Hadamard mechanism support in client-server creation

--- hierarchical_mechanism_LDP/ldp_protocol.py
+++ hierarchical_mechanism_LDP/ldp_protocol.py
@@ -1,6 +1,7 @@
 from pure_ldp.frequency_oracles.local_hashing import LHClient, LHServer
 from pure_ldp.frequency_oracles.direct_encoding import DEClient, DEServer
 from pure_ldp.frequency_oracles.hadamard_response import HadamardResponseClient, HadamardResponseServer
+from pure_ldp.frequency_oracles.hadamard_mechanism import HadamardMechServer, HadamardMechClient
 from pure_ldp.frequency_oracles.unary_encoding import UEClient, UEServer
 
 
@@ -22,38 +23,34 @@ def get_client_server(protocol: str, eps: float, depth: int, b: int) -> tuple:
     # create the clients and servers for each level of the tree, not for the root
     for level in range(1, depth):
         D = int(b ** level)
-        if D > 10_000:
-            # apply hadamard response for large D
+        # ------------- Local Hashing
+        if protocol == 'local_hashing':
+            clients.append(LHClient(epsilon=eps, d=D, use_olh=True))
+            servers.append(LHServer(epsilon=eps, d=D, use_olh=True))
+
+        # ------------- Direct Encoding
+        elif protocol == 'direct_encoding':
+            clients.append(DEClient(epsilon=eps, d=D))
+            servers.append(DEServer(epsilon=eps, d=D))
+
+        # ------------- Hadamard Response
+        elif protocol == 'hadamard_response':
             server = HadamardResponseServer(epsilon=eps, d=D)
             servers.append(server)
             clients.append(HadamardResponseClient(epsilon=eps, d=D, hash_funcs=server.get_hash_funcs()))
-            # apply local hashing
-            # clients.append(LHClient(epsilon=eps, d=D, use_olh=True))
-            # servers.append(LHServer(epsilon=eps, d=D, use_olh=True))
+
+        elif protocol == 'hadamard_mech':
+            server = HadamardMechServer(epsilon=eps, d=D, use_optimal_t=True, t=None)
+            servers.append(server)
+            clients.append(HadamardMechClient(epsilon=eps, d=D, use_optimal_t=True, t=None))
+
+        # ------------- Unary Encoding
+        elif protocol == 'unary_encoding':
+            clients.append(UEClient(epsilon=eps, d=D, use_oue=True))
+            servers.append(UEServer(epsilon=eps, d=D, use_oue=True))
+
         else:
-            # ------------- Local Hashing
-            if protocol == 'local_hashing':
-                clients.append(LHClient(epsilon=eps, d=D, use_olh=True))
-                servers.append(LHServer(epsilon=eps, d=D, use_olh=True))
-
-            # ------------- Direct Encoding
-            elif protocol == 'direct_encoding':
-                clients.append(DEClient(epsilon=eps, d=D))
-                servers.append(DEServer(epsilon=eps, d=D))
-
-            # ------------- Hadamard Response
-            elif protocol == 'hadamard_response':
-                server = HadamardResponseServer(epsilon=eps, d=D)
-                servers.append(server)
-                clients.append(HadamardResponseClient(epsilon=eps, d=D, hash_funcs=server.get_hash_funcs()))
-
-            # ------------- Unary Encoding
-            elif protocol == 'unary_encoding':
-                clients.append(UEClient(epsilon=eps, d=D, use_oue=True))
-                servers.append(UEServer(epsilon=eps, d=D, use_oue=True))
-
-            else:
-                raise ValueError(
-                    f"Protocol {protocol} not recognized, try 'local_hashing', 'direct_encoding' or 'hadamard_response'"
-                )
+            raise ValueError(
+                f"Protocol {protocol} not recognized, try 'local_hashing', 'direct_encoding' or 'hadamard_response'"
+            )
     return clients, servers
